1b9bec7b577910a87b058cacc34f768983a260f3
Ose-Augustine <osebeckley@gmail.com>
2022-06-05 18:18:48 +0100

    function to paginate question

--- 02_trivia_api/starter/backend/flaskr/__init__.py
+++ 02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,25 +8,38 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request,selection):
+  page = request.args.get("page",1,type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE 
+  end   = start + QUESTIONS_PER_PAGE 
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+  return current_questions 
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
   CORS(app,resources={r"/api/*":{"origins":"*"}})
 
-  @app.after_request
-  def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers','Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods','GET, POST, PATCH,DELETE,OPTIONS')
+  # @app.after_request
+  # def after_request(response):
+  #   response.headers.add('Access-Control-Allow-Headers','Content-Type,Authorization')
+  #   response.headers.add('Access-Control-Allow-Methods','GET, POST, PATCH,DELETE,OPTIONS')
   
-
- 
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def all_categories():
+    categories = Category.query.all()
+
+    return jsonify({
+      "categories":categories
+    })
 
 
   '''
