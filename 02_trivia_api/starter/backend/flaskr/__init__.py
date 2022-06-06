faf4b1d81140acd3f79ae6b0d2fd28935c1344d0
Ose-Augustine <osebeckley@gmail.com>
2022-06-06 19:30:16 +0100

    Modifying handler for categories route

--- 02_trivia_api/starter/backend/flaskr/__init__.py
+++ 02_trivia_api/starter/backend/flaskr/__init__.py
@@ -24,10 +24,11 @@ def create_app(test_config=None):
   setup_db(app)
   CORS(app,resources={r"/api/*":{"origins":"*"}})
 
-  # @app.after_request
-  # def after_request(response):
-  #   response.headers.add('Access-Control-Allow-Headers','Content-Type,Authorization')
-  #   response.headers.add('Access-Control-Allow-Methods','GET, POST, PATCH,DELETE,OPTIONS')
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers','Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods','GET, POST, PATCH,DELETE,OPTIONS')
+    return response
   
 
   '''
